ddbc5a3777fe611454edf3c9ec1cd502401afc6d
Roger Walker <roger.john.walker@gmail.com>
2019-02-10 20:35:54 -0500

    Added each of the base data objects, and a dbcontext.

---
+++ Expenses/Expenses.Data/Expenses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expenses.Data
+{
+    public class Expenses
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public Location Location { get; set; }
+
+        public Category Category { get; set; }
+
+        public Account Account { get; set; }
+    }
+}
---
+++ Expenses/Expenses.Data/Location.cs
@@ -0,0 +1,13 @@
+﻿namespace Expenses.Data
+{
+    public class Location
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Category DefaultCategory { get; set; }
+
+        public Account Account { get; set; }
+    }
+}
